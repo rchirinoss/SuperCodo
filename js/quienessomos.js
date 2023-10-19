95586d9b9ee4703f9593e117378c5ce229ba7dfd
Renzo Chirinos <rchirinos@live.com.ar>
2023-10-19 14:49:53 -0300

    primer commit

---
+++ js/quienessomos.js
@@ -0,0 +1,44 @@
+cad=`
+<div id="contentqs">
+    <div class="quienes">
+        <div class="caro">
+            <img src="./images/main/mujer.webp" alt="caro">
+            <p><b>Nombre:</b> Carolina</p>
+            <p><b>Apellido:</b> Briguez</p>
+            <p><b>Edad:</b> 21 años</p>
+            <p><b>Ocupación:</b> Estudiante</p>
+            <p><b>Educación:</b> Potrero Digital - UTN Mar del Plata</p>
+            <p><b>Lenguajes:</b> HTML, CSS, PHP, JAVASCRIP</p>                       
+        </div>
+        <div class="estefi">
+            <img src="./images/main/mujer.webp" alt="estefi">
+            <p><b>Nombre:</b> Estefania</p>
+            <p><b>Apellido:</b> Leiba</p>
+            <p><b>Edad:</b>  </p>
+            <p><b>Ocupación:</b> </p>
+            <p><b>Educación:</b> </p>
+            <p><b>Lenguajes:</b> </p> 
+        </div>
+        <div class="romy">
+            <img src="./images/main/romy.webp" alt="romy">
+            <p><b>Nombre:</b> Romina</p>
+            <p><b>Apellido:</b> Schab</p>
+            <p><b>Edad:</b> 41 años</p>
+            <p><b>Ocupación:</b> Profesora de Ingles</p>
+            <p><b>Educación:</b> Tecnica Superior en Programacion - UTN</p>
+            <p><b>Lenguajes:</b> HTML, CSS, JAVASCRIP, GITHUB, VUE</p> 
+        </div>
+        <div class="renzo">
+            <img src="./images/main/renzo.webp" alt="renzo">
+            <p><b>Nombre:</b> Renzo</p>
+            <p><b>Apellido:</b> Chirinos</p>
+            <p><b>Edad:</b> 36 años</p>
+            <p><b>Ocupación:</b> Estudiante</p>
+            <p><b>Educación:</b> Desarrollo Web - Universidad Kennedy </p>
+            <p><b>Lenguajes:</b> HTML, CSS, JAVASCRIP, C++, PYTHON</p> 
+        </div>
+    </div>
+</div>
+`
+
+document.querySelector("main").innerHTML = cad 
---
+++ js/sucursales.js
@@ -0,0 +1,13 @@
+cad=`
+<div id="sucursales">
+    <div class="suc1">
+    <h2>Pestaña en Mantenimiento<h2>
+
+    </div>
+    <div class="suc2">
+
+    </div>
+</div>
+`
+
+document.querySelector("main").innerHTML = cad
